98aadd169a9ef3722dc28bc7582325681af58472
Jose Cortes <josepablocortes@gmail.com>
2014-08-25 01:16:40 -0300

    Funciona, falta hacer profiling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <map>
+#include <sstream>
 
 using namespace std;
 
@@ -11,7 +13,7 @@ using namespace std;
 */
 
 typedef vector<unsigned int> mySet;
-typedef vector<mySet> collection;
+typedef map<string, mySet> collection;
 
 bool isPrimo(unsigned int number);
 collection makeSet(mySet * set);
@@ -19,32 +21,34 @@ collection makeSet(mySet * set);
 
 int main()
 {
-
     mySet elementos;
-    for(unsigned int i = 1; i< 5; ++i) elementos.push_back(i);
-
+    for(unsigned int i = 1; i < 4; ++i){
+        elementos.push_back(i);
+    }
     collection conj = makeSet( &elementos );
-
     for (collection::iterator it = conj.begin(); it != conj.end(); ++it){
-        cout << "[";
-        mySet m2 = *it;
+        cout << "["<< it->first << "] = ";
+        mySet m2 = it->second;
         for (mySet::iterator it2 = m2.begin(); it2 != m2.end(); ++it2){
             cout << *it2 << " ";
         }
-        cout << "]" << endl;
+        cout  << endl;
     }
-
-
     return 0;
 }
 
 
 
 bool isPrimo(unsigned int number){
-    if (number <= 2) return true;
-    if ( !(number % 2) && number > 2 ) return false;
+
+    if (number <= 2){
+        return true;
+    }
+    if ( !(number % 2) && number > 2 ) {
+        return false;
+    }
     bool result = true;
-    for (unsigned int i = 3 ; i < ((unsigned int)sqrt( number) + 1); ++i)
+    for (unsigned int i = 3 ; i < ((unsigned int)sqrt( number ) + 1); ++i)
     {
         if ( !(number % i) ) {
             result = false;
@@ -52,32 +56,39 @@ bool isPrimo(unsigned int number){
         }
     }
     return result;
-
 }
 
 bool sumAndCheck(mySet & set){
     unsigned int sum = 0;
-    for(mySet::iterator)
+    for(mySet::iterator it = set.begin(); it != set.end(); ++it){
+        sum += *it;
+    }
+    return isPrimo( sum );
 }
 
 collection makeSet(mySet * set){
     collection conjunto;
+
     for (mySet::iterator it = set->begin(); it != set->end(); ++it){
         mySet sub;
+        stringstream key;
         for (mySet::iterator it2 = set->begin(); it2 != set->end(); ++it2){
             if (it2 != it ){
+                key << *it2 << ".";
                 sub.push_back(*it2);
             }
         }
-        conjunto.push_back( sub ); // antes sumá y checká que sea primo
-        if( sub.size() > 1 ) {
-            collection tmp = makeSet( &sub );
-            conjunto.insert( conjunto.end(), tmp.begin(),tmp.end() );
+
+
+        if(sumAndCheck( sub )){
+            conjunto[ key.str() ] = sub;
         }
 
+        if( sub.size() > 1 ) {
+            collection tmp = makeSet( &sub );
+            conjunto.insert( tmp.begin(),tmp.end() );
+        }
     }
+
     return conjunto;
 }
-
-
-
